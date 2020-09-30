544e1be7cb867738f10b3dca4d186bcb4ccfdd63
DOGUK <dgkim3811@naver.com>
2020-10-01 05:32:29 +0900

    AddLayout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from 'react';
+import { Helmet } from 'react-helmet-async';
+
+type Props = {
+  children?: ReactNode;
+  title?: string;
+};
+
+const Layout = ({ children, title }: Props) => {
+  return (
+    <>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      {children}
+    </>
+  );
+};
+
+export default Layout;
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from 'components/Layout/index';
+
+const IndexPage = () => {
+  return <Layout title="트위터 클론"></Layout>;
+};
+
+export default IndexPage;
