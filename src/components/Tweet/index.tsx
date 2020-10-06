d34209038cbd60ef440a0505fe1868a7e06c3034
DOGUK <dgkim3811@naver.com>
2020-10-06 13:14:38 +0900

    AddDeleteLogicAtTweetComponent

--- src/components/Tweet/index.tsx
+++ src/components/Tweet/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-
+import { firestore } from 'firebase';
 type Props = {
   tweetObj?: any;
   isOwner?: any;
 };
 
 const TweetComponent: React.FC<Props> = ({ tweetObj, isOwner }) => {
+  const onDeleteClick = async () => {
+    const ok = window.confirm(`Are you sure u want to del it?`);
+    if (ok) {
+      await firestore().doc(`tweets/${tweetObj.id}`).delete();
+    }
+  };
   return (
     <>
       <div key={tweetObj.id}>{tweetObj.text}</div>
       {isOwner && (
         <>
-          <button>Delete</button>
+          <button onClick={onDeleteClick}>Delete</button>
           <button>Edit</button>
         </>
       )}
